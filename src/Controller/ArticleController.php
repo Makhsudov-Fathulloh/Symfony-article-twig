18c4ede36312f55faa430feb405863ff4a490f74
Makhsudov <mr.makhsudov@gmail.com>
2023-03-22 19:30:08 +0500

    Add bootstrap, webpack-bundle

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,11 +12,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/article', name: 'article')]
     public function index(): Response
+    /* Method 2 public function index(ArticleRepository $articleRepository): Response
+     {
+         $article = $articleRepository->findAll(); */
     {
-        return $this->render('index.html.twig', [
-            'title' => 'Hello World'
-        ]);
+        //Sql
+        //findAll() - SELECT * FROM movies;
+        //find() - SELECT * from movies WHERE id = 1;
+        //findBy() - SELECT * FROM movies ORDER BY id DESC;
+        //findOneBy() - SELECT * from movies WHERE id = 1 AND title = 'Lorem ipsum 2'
+        //count() - SELECT COUNT(id) FROM movies
+
+        /* $repository = $this->em->getRepository(Article::class);
+        $article = $repository->findAll(); */
+
+        //Commands
+        //$article = $this->repository->findAll();
+        //$article = $this->repository->find(1);
+        //$article = $this->repository->findBy([], ['id' => 'DESC']);
+        //$article = $this->repository->findOneBy(['id' => 1, 'title' => 'Lorem ipsum 2', []]);
+        //$article = $this->repository->count([]);
+
+        return $this->render('index.html.twig');
     }
 }
