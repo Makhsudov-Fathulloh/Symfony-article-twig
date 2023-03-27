481ddc69a1b2fbd9f557516cb0b1a741c527e228
Makhsudov <mr.makhsudov@gmail.com>
2023-03-27 06:30:53 +0500

    Add file show.html.twig and create.html.twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleFormType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,6 +24,7 @@ class ArticleController extends AbstractController
         $this->articleRepository = $articleRepository;
     }
 
+
     #[Route('/article', methods: ['GET'], name: 'articles')]
     public function index(): Response
     {
@@ -32,7 +36,45 @@ class ArticleController extends AbstractController
     }
 
 
-    #[Route('/article{id}', methods: ['GET'], name: 'show')]
+    #[Route('/articles/create', name: 'create_article')]
+    public function create(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newArticle = $form->getData();
+
+            $imagePath = $form->get('image_path')->getData();
+            if ($imagePath){
+                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+
+                try {
+                    $imagePath->move(
+                      $this->getParameter('kernel.project_dir') . '/public/uploads',
+                      $newFileName
+                    );
+                }catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $newArticle->setImagePath('/uploads/' . $newFileName);
+            }
+
+            $this->em->persist($newArticle);
+            $this->em->flush();
+
+            return $this->redirectToRoute('articles');
+        }
+
+        return $this->render('articles/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/articles/{id}', methods: ['GET'], name: 'show_article')]
     public function show($id): Response
     {
         $article = $this->articleRepository->find($id);
